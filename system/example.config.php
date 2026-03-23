72a69981d064b64b312658cec1b4c7c4135c300b
Martin Tonek <martin@tonek.se>
2026-03-23 10:48:49 +0100

    Fixes

---
+++ system/example.config.php
@@ -0,0 +1,14 @@
+<?php
+define('ROOTPATH', str_replace("/system","",dirname(__FILE__) . "/"));
+define('WWWPATH', "https://********/"); // with trailing slash
+
+$host = '********';
+$db   = '********';
+$user = '********';
+$pass = '********';
+
+$mysqli = new mysqli($host, $user, $pass, $db);
+if ($mysqli->connect_errno) {
+    die("DB-fel: " . $mysqli->connect_error);
+}
+$mysqli->set_charset('utf8mb4');
